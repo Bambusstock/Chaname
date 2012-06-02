9a9bad8cacf3881cab7bb7ab026f655fd46aff99
fzuellich <fzuellich@gmail.com>
2012-06-02 17:09:22 +0200

    fixed empty config file

--- src/me/Bambusstock/Chaname/Chaname.java
+++ src/me/Bambusstock/Chaname/Chaname.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Chaname extends JavaPlugin{
     public void onEnable() {
 	// Load config
-	this.getConfig().options().copyDefaults(false);
+	this.getConfig().options().copyDefaults(true);
 	this.saveConfig();
 	getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
